458734598344c3dabf301d989171c3af8e323a84
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-03 10:14:28 -0400

    Update lakefile.lean

--- palomar/lakefile.lean
+++ palomar/lakefile.lean
@@ -3,8 +3,8 @@ open Lake DSL
 
 package «DirectProductConjecture» where
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+require mathlib from git 
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.33.0"
 
 @[default_target]
 lean_lib «Challenge» where
