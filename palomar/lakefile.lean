905763711c98e7a818de7b3502430c0a55036b4f
Jonathan ƒ(n) Reed <jonathanalanreed1983@gmail.com>
2026-09-02 21:28:21 -0400

    Update lakefile.lean

--- palomar/lakefile.lean
+++ palomar/lakefile.lean
@@ -7,5 +7,8 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «DirectProductConjecture» where
+lean_lib «Challenge» where
+  srcDir := "."
+
+lean_lib «Solution» where
   srcDir := "."
